abfca71625333820f0adcf6694eb9399b0a0c06a
Sutin <poly.dev2018@gmail.com>
2023-07-24 17:07:00 +0700

    feat: update gitignore

--- styled-system/tokens/index.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import type { Token } from './tokens';
-
-export declare const token: {
-	(path: Token, fallback?: string): string;
-	var: (path: string, fallback?: string) => string;
-};
-
-export * from './tokens';
