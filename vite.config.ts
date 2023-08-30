0670bd5aa16ede5185adf3d51adcf30d21a05242
Sutin <poly.dev2018@gmail.com>
2023-08-30 16:38:45 +0700

    feat: upgrade panda & TS v5

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import externalize from 'vite-plugin-externalize-dependencies';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), externalize({ externals: ['externalized-dependency'] })],
 	server: {
 		fs: {
 			allow: ['styled-system']
@@ -10,5 +13,16 @@ export default defineConfig({
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	build: {
+		sourcemap: true,
+		rollupOptions: {
+			external: 'moment'
+		}
+	},
+	resolve: {
+		alias: {
+			moment: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js'
+		}
 	}
 });
