3e0f610b616ab3da9c0b59d512563dc7bd8af560
Sutin <poly.dev2018@gmail.com>
2023-08-30 17:15:16 +0700

    fix: update paths for prod

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
@@ -12,7 +14,8 @@ const config = {
 			'@css': 'node_modules/tiny-panda/css',
 			'@patterns': 'node_modules/tiny-panda/patterns',
 			'@tokens': 'node_modules/tiny-panda/tokens'
-		}
+		},
+		paths: dev ? '' : 'https://github.com/sutin1234/sveltekit-4-pandaCSS'
 	}
 };
 
